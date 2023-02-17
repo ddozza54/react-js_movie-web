1405caf2b2561146624543170291e037a23d8c1e
UHquokka <100435210+UHquokka@users.noreply.github.com>
2023-02-17 22:00:30 +0900

    data에서 movies 가져오기

--- src/App.js
+++ src/App.js
@@ -2,26 +2,22 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(true);
-  const [coins, setConins] = useState([]);
-  useEffect(() => {
-    fetch("https://api.coinpaprika.com/v1/tickers")
-      .then((response) => response.json())
-      .then((json) => setConins(json)); //데이터를 받아옴. 코인정보 -> 변경된 coins에 들어감.
+  const [movies, setMovies] = useState([]);
+
+  const getMovies = async () => {
+    const json = await (
+      await fetch(
+        `https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`
+      )
+    ).json();
+    setMovies(json.data.movies);
     setLoading(false);
+  };
+  useEffect(() => {
+    getMovies();
   }, []);
-  return (
-    <>
-      <h1>The Coins!({coins.length})</h1>
-      {loading ? <strong>Loading...</strong> : null}
-      <ul>
-        {coins.map((coin, id) => (
-          <li key={id}>
-            {coin.name} ({coin.symbol} : ${coin.quotes.USD.price} USD)
-          </li>
-        ))}
-      </ul>
-    </>
-  );
+  console.log(movies);
+  return <>{loading ? <h1>Loading...</h1> : null}</>;
 }
 
 export default App;
