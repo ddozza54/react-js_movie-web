063fbe8daae5b80937bdbcb158e3958e969202b4
UHquokka <100435210+UHquokka@users.noreply.github.com>
2023-02-20 14:21:19 +0900

    글자수로 summary 자르기

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -10,7 +10,7 @@ function Movie({ id, coverImg, title, summary, genres }) {
         <h1>
           <Link to={`/movie/${id}`}>{title}</Link>
         </h1>
-        <p>{summary}</p>
+        <p>{summary.length > 235 ? `${summary.slice(0, 235)}...` : summary}</p>
         <ul>
           {genres.map((genre) => (
             <li key={genre}>{genre}</li>
