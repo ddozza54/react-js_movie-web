156e05ad194cc26ad6ef1c169eeafae59604a615
UHquokka <100435210+UHquokka@users.noreply.github.com>
2023-02-20 15:59:59 +0900

    자식 요소에도 스타일 주기 & pseudo selector 만들기

--- src/App.js
+++ src/App.js
@@ -1,38 +1,44 @@
-import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
-const Father = styled.div`
-  display: flex;
-`;
-const Box = styled.div`
-  background-color: ${(props) => props.bgColor};
-  width: 100px;
-  height: 100px;
-`;
-const Circle = styled(Box)`
-  border-radius: 50px;
-`;
-const Btn = styled.button`
-  color: white;
-  background-color: gold;
-  width: 100px;
-  height: 50px;
+const Wrapper = styled.div``;
+
+const roatation = keyframes`
+0%{
+  transform:rotate(0deg);
   border-radius: 15px;
-  border: none;
+}
+50%{
+  border-radius: 50%;
+}
+100%{
+  transform:rotate(360deg);
+  border-radius: 15px;
+}
 `;
 
-const Input = styled.input.attrs({ required: true })`
-  background-color: tomato;
-`;
 function App() {
+  const Box = styled.div`
+    height: 200px;
+    width: 200px;
+    background-color: gold;
+    animation: ${roatation} 2s linear infinite;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    span {
+      font-size: 50px;
+      &:hover {
+        font-size: 100px;
+      }
+    }
+  `;
+
   return (
-    <Father>
-      <Box bgColor="teal"></Box>
-      <Circle bgColor="tomato"></Circle>
-      <Btn></Btn>
-      <Btn as="input"></Btn>
-      <Input></Input>
-      <Input></Input>
-    </Father>
+    <Wrapper>
+      <Box>
+        <span>🌼</span>
+      </Box>
+    </Wrapper>
   );
 }
 
