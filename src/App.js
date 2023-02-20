4db03c645193f2d193b5fd15f33babe9712765ae
UHquokka <100435210+UHquokka@users.noreply.github.com>
2023-02-20 15:26:53 +0900

    as 를 이용해서 같은 style의 다른 element 만들기, attrs로 중복되는 속성 추가하기

--- src/App.js
+++ src/App.js
@@ -11,11 +11,27 @@ const Box = styled.div`
 const Circle = styled(Box)`
   border-radius: 50px;
 `;
+const Btn = styled.button`
+  color: white;
+  background-color: gold;
+  width: 100px;
+  height: 50px;
+  border-radius: 15px;
+  border: none;
+`;
+
+const Input = styled.input.attrs({ required: true })`
+  background-color: tomato;
+`;
 function App() {
   return (
     <Father>
       <Box bgColor="teal"></Box>
       <Circle bgColor="tomato"></Circle>
+      <Btn></Btn>
+      <Btn as="input"></Btn>
+      <Input></Input>
+      <Input></Input>
     </Father>
   );
 }
