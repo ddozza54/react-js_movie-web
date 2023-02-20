0c1cde85ed8e7afa4ec68c676983c51be5acc4f7
UHquokka <100435210+UHquokka@users.noreply.github.com>
2023-02-20 14:52:12 +0900

    style-component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
-import { useEffect, useState } from "react";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import Detail from "./routes/Detail";
-import Home from "./routes/Home";
+import styled from "styled-components";
+
+const Father = styled.div`
+  display: flex;
+`;
+const BoxOne = styled.div`
+  background-color: teal;
+  width: 100px;
+  height: 100px;
+`;
+const BoxTwo = styled.div`
+  background-color: tomato;
+  width: 100px;
+  height: 100px;
+`;
+const Text = styled.span`
+  color: white;
+`;
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/movie/:id">
-          <Detail />
-        </Route>
-        /** home으로 가는 router*/
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <Father>
+      <BoxOne>
+        <Text>Hello</Text>
+      </BoxOne>
+      <BoxTwo></BoxTwo>
+    </Father>
   );
 }
 
